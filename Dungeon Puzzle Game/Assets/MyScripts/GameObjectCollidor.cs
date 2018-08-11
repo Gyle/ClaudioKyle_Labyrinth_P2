b33835c67439ab90a124a18ede596261c6e3147b
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-08-12 11:47:46 +1200

    manual rebase from patch branch.

---
+++ Dungeon Puzzle Game/Assets/MyScripts/GameObjectCollidor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class GameObjectCollidor : MonoBehaviour {
+	
+	public delegate void EventHandler(GameObjectCollidor sender);
+
+	public static event EventHandler playerEnteredCollision = null;
+	public static event EventHandler playerExitedCollision = null;
+	public static event EventHandler playerEnteredTrigger = null;
+	public static event EventHandler playerExitedTrigger = null;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.GetComponentInChildren<PlayerAvatar>() != null)
+		{
+			if (playerEnteredCollision != null)
+				playerEnteredCollision(this);
+				print("entered goal");
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.GetComponentInChildren<PlayerAvatar>() != null)
+		{
+			if (playerExitedCollision != null)
+				playerExitedCollision(this);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.GetComponent<PlayerAvatar>() != null)
+		{
+			if (playerEnteredTrigger != null)
+			{
+				playerEnteredTrigger(this);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.GetComponentInChildren<PlayerAvatar>() != null)
+		{
+			if (playerExitedTrigger != null)
+				playerExitedTrigger(this);
+		}
+	}
+
+	
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# 18.2 COMP313 Assignment 2
+## Student: Kyle Claudio, Lucas Gosling
+
+## Burndown Chart
+http://radekstepan.com/burnchart/#!/
+
+To show: 
+1) Log into GitHub and put in the path to this Repo.
+2) Select add project on top left
+3) enter path. EG: Gyle/COMP313_Unity_Game
